77d2f496105d2c52ce7165cafde3ecf30757eba6
Dharsan-Arunkumar-V-P <shadeddak69@gmail.com>
2025-01-03 09:20:28 +0530

    Update index.html and scripts.js, rename README.md to docs

--- scripts.js
+++ scripts.js
@@ -1,10 +1,14 @@
 document.getElementById("myForm").addEventListener("submit", function(event) {
   event.preventDefault();
-  const name = document.getElementById("name").value;
-  const email = document.getElementById("email").value;
+  const name = document.getElementById("name").value.trim();
+  const email = document.getElementById("email").value.trim();
 
   if (name && email) {
-      alert("Thank you, " + name + "! Your form has been submitted.");
+
+      document.getElementById("displayName").textContent = "Name: " + name;
+      document.getElementById("displayEmail").textContent = "Email: " + email;
+      document.getElementById("submittedInfo").style.display = "block";
+      document.getElementById("myForm").reset();
   } else {
       alert("Please fill out all fields.");
   }
