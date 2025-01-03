d4b1dab2421755de1e9fac7c96d4797811367d57
Dharsan Arunkumar  V P <shadeddak69@gmail.com>
2025-01-03 09:55:04 +0530

    upgraded the whole website

--- scripts.js
+++ scripts.js
@@ -1,13 +1,11 @@
-document.getElementById("myForm").addEventListener("submit", function(event) {
+document.getElementById("contactForm").addEventListener("submit", function(event) {
   event.preventDefault();
   const name = document.getElementById("name").value.trim();
   const email = document.getElementById("email").value.trim();
 
   if (name && email) {
-      document.getElementById("displayName").textContent = "Name: " + name;
-      document.getElementById("displayEmail").textContent = "Email: " + email;
-      document.getElementById("submittedInfo").style.display = "block";
-      document.getElementById("myForm").reset();
+      alert("Thank you for reaching out, " + name + "!");
+      document.getElementById("contactForm").reset();
   } else {
       alert("Please fill out all fields.");
   }
